81d191a3e459f6bdd1d0ddc009277a20416c71ce
Michael Byrd <23690719+thealchemist2016@users.noreply.github.com>
2020-09-01 02:46:35 -0500

    semicolons added

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+/*jshint esversion: 6 */
 const express = require('express');
 const app = express();
 
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
-]
+];
 
 app.get('/users',function(req,res){
 	res.json({
 		success: true,
 		message: 'successfully got users. Nice!',
 		users: mockUserData
-	})
-})
+	});
+});
 
 app.get('/users/:id',function(req,res){
-	console.log(req.params.id)
+	console.log(req.params.id);
 	res.json({
 		success: true,
 		message: 'got one user',
 		user: req.params.id
-	})
-})
-app.listen(8000,function(){console.log('server is listening')})
+	});
+});
+app.listen(8000,function(){console.log('server is listening');});
